bee1716d6ab99b7b667bb9c0ecf94fd4f1bf3af4
levy002 <levy.ukwi002@gmail.com>
2022-02-14 13:25:59 +0200

    Add .js file entry point for all modules

---
+++ index.js
@@ -0,0 +1,12 @@
+import { initStorage, getAddedBook, form } from './modules/displayBooks.js';
+import {
+  updateView, listTag, newTag, contactTag,
+} from './modules/navigations.js';
+import displayDate from './modules/time.js';
+
+initStorage();
+form.addEventListener('submit', getAddedBook);
+listTag.addEventListener('click', () => { updateView(listTag.id); });
+newTag.addEventListener('click', () => { updateView(newTag.id); });
+contactTag.addEventListener('click', () => { updateView(contactTag.id); });
+setInterval(displayDate, 1000);
